f3c28eeb15724e2fb3ca926c402a25492551124c
Egor <112934317+Forehm@users.noreply.github.com>
2023-06-07 22:05:57 +0300

    Update SmartPointer.h

--- SmartPointer.h
+++ SmartPointer.h
@@ -25,31 +25,38 @@ namespace forehm
             delete ptr_;
         }
 
-        T* GetRawPtr() const noexcept {
+        T* GetRawPtr() const noexcept 
+        {
             return ptr_;
         }
 
-        T* Release() noexcept {
+        T* Release() noexcept 
+        {
             T* p = ptr_;
             ptr_ = nullptr;
             return p;
         }
 
-        explicit operator bool() const {
+        explicit operator bool() const 
+        {
             return ptr_ != nullptr;
         }
 
-        T* operator->() const {
+        T* operator->() const 
+        {
             using namespace std::literals;
-            if (!ptr_) {
+            if (!ptr_) 
+            {
                 throw std::logic_error("Scoped ptr is null"s);
             }
             return ptr_;
         }
 
-        T& operator*() const {
+        T& operator*() const 
+        {
             using namespace std::literals;
-            if (!ptr_) {
+            if (!ptr_) 
+            {
                 throw std::logic_error("Scoped ptr is null"s);
             }
             return *ptr_;
